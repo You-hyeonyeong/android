e4378496efa2732e8ff9c91dc59e693df7fc7685
Kimhyunhee <on.aior94@gmail.com>
2018-07-02 22:42:55 +0900

    스플래쉬 완료

--- app/src/main/java/org/weatherook/weatherook/ui/activity/SplashActivity.kt
+++ app/src/main/java/org/weatherook/weatherook/ui/activity/SplashActivity.kt
@@ -1,7 +1,11 @@
 package org.weatherook.weatherook.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 import org.weatherook.weatherook.R
 
 class SplashActivity : AppCompatActivity() {
@@ -9,5 +13,16 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val source = findViewById<ImageView>(R.id.splash_gif)
+        Glide.with(this)
+                .load(R.drawable.splash_img)
+                .into(source)
+
+        val handler = Handler()
+        handler.postDelayed({
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+            finish()
+        }, 2000)
     }
 }
