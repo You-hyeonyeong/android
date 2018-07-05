24551ba48cf69f297c310f18417ca3ad94b83f08
jbw1003 <jbw10032@gmail.com>
2018-07-06 00:30:23 +0900

    현희누나가 홈화면 만듦

--- app/src/main/java/org/weatherook/weatherook/ui/fragment/HomeFragment.kt
+++ app/src/main/java/org/weatherook/weatherook/ui/fragment/HomeFragment.kt
@@ -4,22 +4,42 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_home.*
 import org.weatherook.weatherook.R
-import org.weatherook.weatherook.ui.adapter.RecommendAdapter
-import org.weatherook.weatherook.ui.adapter.WeatherAdapter
+import org.weatherook.weatherook.adapter.FollowingAdapter
+import org.weatherook.weatherook.adapter.RecommendAdapter
+import org.weatherook.weatherook.adapter.WeatherAdapter
+import org.weatherook.weatherook.ui.item.FollowingItem
 import org.weatherook.weatherook.ui.item.RecommendItem
 import org.weatherook.weatherook.ui.item.WeatherItem
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), View.OnClickListener {
+
+    private var isWeatherButtons: Boolean = false
+    override fun onClick(v: View?) {
+
+        if (!isWeatherButtons) {
+            isWeatherButtons = true
+            home_weather_alarm.visibility = View.INVISIBLE
+            home_weather_recycler.visibility = View.VISIBLE
+
+        } else {
+            isWeatherButtons = false
+            home_weather_alarm.visibility = View.VISIBLE
+            home_weather_recycler.visibility = View.INVISIBLE
+        }
+    }
+
     lateinit var weatherItems : ArrayList<WeatherItem>
     lateinit var weatherAdapter : WeatherAdapter
     lateinit var recommendItems : ArrayList<RecommendItem>
     lateinit var recommendAdapter : RecommendAdapter
-
+    lateinit var followingItems: ArrayList<FollowingItem>
+    lateinit var followingAdapter: FollowingAdapter
         override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view : View = View.inflate(activity, R.layout.fragment_home, null)
         return view
@@ -28,22 +48,24 @@ class HomeFragment : Fragment() {
     override fun onStart() {
         super.onStart()
 
+        home_weather_change.setOnClickListener(this)
+
         weatherItems = ArrayList()
 
-        weatherItems.add(WeatherItem("지금",R.drawable.abc_ab_share_pack_mtrl_alpha,"25º"))
-        weatherItems.add(WeatherItem("지금",R.drawable.abc_ab_share_pack_mtrl_alpha,"25º"))
-        weatherItems.add(WeatherItem("지금",R.drawable.abc_ab_share_pack_mtrl_alpha,"25º"))
-        weatherItems.add(WeatherItem("지금",R.drawable.abc_ab_share_pack_mtrl_alpha,"25º"))
-        weatherItems.add(WeatherItem("지금",R.drawable.abc_ab_share_pack_mtrl_alpha,"25º"))
-        weatherItems.add(WeatherItem("지금",R.drawable.abc_btn_borderless_material,"25º"))
-        weatherItems.add(WeatherItem("지금",R.drawable.abc_ab_share_pack_mtrl_alpha,"25º"))
-        weatherItems.add(WeatherItem("지금",R.drawable.abc_ab_share_pack_mtrl_alpha,"25º"))
-        weatherItems.add(WeatherItem("지금",R.drawable.abc_ab_share_pack_mtrl_alpha,"25º"))
-        weatherItems.add(WeatherItem("지금",R.drawable.abc_ab_share_pack_mtrl_alpha,"25º"))
-        weatherItems.add(WeatherItem("지금",R.drawable.abc_ab_share_pack_mtrl_alpha,"25º"))
-        weatherItems.add(WeatherItem("지금",R.drawable.abc_ab_share_pack_mtrl_alpha,"25º"))
-
+        weatherItems.add(WeatherItem("지금", R.drawable.main_sun2, "25º"))
+        weatherItems.add(WeatherItem("1시", R.drawable.main_sun2, "25º"))
+        weatherItems.add(WeatherItem("2시", R.drawable.main_rain_2, "25º"))
+        weatherItems.add(WeatherItem("3시", R.drawable.main_rain_2, "25º"))
+        weatherItems.add(WeatherItem("4시", R.drawable.main_sun2, "25º"))
+        weatherItems.add(WeatherItem("5시", R.drawable.main_cloud_sun_2, "25º"))
+        weatherItems.add(WeatherItem("6시", R.drawable.main_sun2, "25º"))
+        weatherItems.add(WeatherItem("1시", R.drawable.main_sun2, "25º"))
+        weatherItems.add(WeatherItem("2시", R.drawable.main_rain_2, "25º"))
+        weatherItems.add(WeatherItem("3시", R.drawable.main_rain_2, "25º"))
+        weatherItems.add(WeatherItem("4시", R.drawable.main_sun2, "25º"))
+        weatherItems.add(WeatherItem("지금", R.drawable.main_cloud_sun_2, "25º"))
         weatherAdapter = WeatherAdapter(weatherItems)
+        weatherAdapter.setOnItemClickListener(this)
         home_weather_recycler.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL,false)
         home_weather_recycler.adapter = weatherAdapter
 
@@ -53,15 +75,25 @@ class HomeFragment : Fragment() {
         recommendItems.add(RecommendItem(R.drawable.heartcolor))
         recommendItems.add(RecommendItem(R.drawable.heartcolor))
         recommendItems.add(RecommendItem(R.drawable.heartcolor))
-        recommendItems.add(RecommendItem(R.drawable.heartcolor))
-        recommendItems.add(RecommendItem(R.drawable.heartcolor))
-        recommendItems.add(RecommendItem(R.drawable.heartcolor))
-        recommendItems.add(RecommendItem(R.drawable.heartcolor))
-        recommendItems.add(RecommendItem(R.drawable.heartcolor))
-        recommendItems.add(RecommendItem(R.drawable.heartcolor))
 
-        recommendAdapter = RecommendAdapter(recommendItems)
+        recommendAdapter = RecommendAdapter(recommendItems, context!!)
+        //     recommendAdapter.setOnItemClickListener(this)
         home_recommend_recycler.layoutManager = GridLayoutManager(context,2)
         home_recommend_recycler.adapter = recommendAdapter
+
+
+        followingItems = ArrayList()
+
+        followingItems.add(FollowingItem(R.drawable.heartcolor, "kim", R.drawable.main_sun))
+        followingItems.add(FollowingItem(R.drawable.heartcolor, "kim", R.drawable.main_cloud_sun_2))
+        followingItems.add(FollowingItem(R.drawable.heartcolor, "kim", R.drawable.main_sun))
+        followingItems.add(FollowingItem(R.drawable.heartcolor, "kim", R.drawable.main_rain))
+
+        followingAdapter = FollowingAdapter(followingItems)
+        //      followingAdapter.setOnItemClickListener(this)
+        home_following_recycler.layoutManager = LinearLayoutManager(context)
+        home_following_recycler.adapter = followingAdapter
+
+
     }
 }
