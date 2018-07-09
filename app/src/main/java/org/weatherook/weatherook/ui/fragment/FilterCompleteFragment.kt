139d009c22b9bf66afb678ec73e771ae97b6a660
HYEON-PC\HYEON <dbgusdud412@naver.com>
2018-07-09 13:45:52 +0900

    필터완료 눌렀을때 + 풀한거

--- app/src/main/java/org/weatherook/weatherook/ui/fragment/FilterCompleteFragment.kt
+++ app/src/main/java/org/weatherook/weatherook/ui/fragment/FilterCompleteFragment.kt
@@ -10,6 +10,7 @@ import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_filter_complete.*
 import org.weatherook.weatherook.R
 import org.weatherook.weatherook.adapter.FollowingAdapter
+import org.weatherook.weatherook.item.CommentItem
 import org.weatherook.weatherook.item.FollowingItem
 
 /**
@@ -27,6 +28,10 @@ class FilterCompleteFragment : Fragment(),View.OnClickListener {
     }
     lateinit var filtercomitems : ArrayList<FollowingItem>
     lateinit var filtercomadapter : FollowingAdapter
+    lateinit var commentItems1 : ArrayList<CommentItem>
+    lateinit var commentItems2 : ArrayList<CommentItem>
+    lateinit var filterLinearLayoutManager: LinearLayoutManager
+
 
     override fun onCreateView(inflater: LayoutInflater, conatiner: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view : View = View.inflate(activity, R.layout.fragment_filter_complete, null)
@@ -36,14 +41,27 @@ class FilterCompleteFragment : Fragment(),View.OnClickListener {
 
     override fun onStart() {
         super.onStart()
-        filtercomitems = ArrayList()
-        filtercomitems.add(FollowingItem(R.drawable.brown, "hiriyo", R.drawable.heart,"100",R.drawable.main_night_2,"7월 25일","맑음","25/31","정빈이는 체고다 정비니 짱짱"))
-        filtercomitems.add(FollowingItem(R.drawable.cony, "프린스 빈", R.drawable.heart,"100",R.drawable.main_rain_2,"7월 26일","흐림","24/31","정빈이는 체고다 정비니 짱짱"))
-        filtercomitems.add(FollowingItem(R.drawable.selly, "정시후", R.drawable.heart,"112",R.drawable.main_snow_2,"7월 27일","맑음","25/31","정빈이는 체고다 정비니 짱짱"))
-        filtercomitems.add(FollowingItem(R.drawable.moon, "hiriyo", R.drawable.heart,"112",R.drawable.main_cloud_sun_2,"7월 2일","맑음","27/31","정빈이는 체고다 정비니 짱짱"))
 
-        filtercomadapter = FollowingAdapter(filtercomitems,context!!)
-        //      followingAdapter.setOnItemClickListener(this)
+        commentItems1 = ArrayList()
+        commentItems2 = ArrayList()
+        filtercomitems = ArrayList()
+
+        commentItems1.add(CommentItem("kim","hihihi"))
+        commentItems1.add(CommentItem("bin","i am bin"))
+        commentItems1.add(CommentItem("young","hihihi"))
+
+        commentItems2.add(CommentItem("young","hihihi"))
+        commentItems2.add(CommentItem("kim","wow"))
+        commentItems2.add(CommentItem("kim","zzzzz"))
+
+        filtercomitems.add(FollowingItem(R.drawable.brown, "hiriyo", R.drawable.heart, "112", R.drawable.main_night_2, "7월 25일", "맑음", "25/31", "정빈이는 체고다 정비니 짱짱", commentItems1))
+        filtercomitems.add(FollowingItem(R.drawable.brown, "프린스 빈", R.drawable.heart, "112", R.drawable.main_rain_2, "7월 26일", "흐림", "24/31", "정빈이는 체고다 정비니 짱짱", commentItems2))
+        filtercomitems.add(FollowingItem(R.drawable.brown, "정시후", R.drawable.heart, "112", R.drawable.main_snow_2, "7월 27일", "맑음", "25/31", "정빈이는 체고다 정비니 짱짱", commentItems1))
+        filtercomitems.add(FollowingItem(R.drawable.brown, "hiriyo", R.drawable.heart, "112", R.drawable.main_cloud_sun_2, "7월 2일", "맑음", "27/31", "정빈이는 체고다 정비니 짱짱", commentItems2))
+
+        filterLinearLayoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+
+        filtercomadapter = FollowingAdapter(filtercomitems,filterLinearLayoutManager,context!!)
         filter_com_recycle.layoutManager = LinearLayoutManager(context)
         filter_com_recycle.adapter = filtercomadapter
 
