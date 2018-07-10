fe4e12defcff88bcda1c40f6bba11d2825344202
Kimhyunhee <on.aior94@gmail.com>
2018-07-11 02:15:50 +0900

    로그인, 회원가입 뷰 수정 및 홈화면 날씨, 코디추천 수정

--- app/src/main/java/org/weatherook/weatherook/ui/activity/SignupActivity.kt
+++ app/src/main/java/org/weatherook/weatherook/ui/activity/SignupActivity.kt
@@ -25,11 +25,9 @@ class SignupActivity : AppCompatActivity(), View.OnClickListener {
 
         signup_close.setOnClickListener(this)
 
-        val dotsIndicator = findViewById<WormDotsIndicator>(R.id.worm_dots_indicator)
         val viewPager = findViewById<ViewPager>(R.id.signup_viewPager)
         val adapter = SignupPagerAdapter(supportFragmentManager)
 
         viewPager.adapter = adapter
-        dotsIndicator.setViewPager(viewPager)
     }
 }
