3e2920fc19b09c26e4a3d60014c8ba09874d5f4e
jbw1003 <jbw10032@gmail.com>
2018-07-13 23:14:37 +0900

    Merge branch 'master' of https://github.com/Weatherook/android
    # Conflicts:
    #	app/src/main/java/org/weatherook/weatherook/api/network/NetworkService.kt
    #	app/src/main/java/org/weatherook/weatherook/ui/activity/CommentActivity.kt
    #	app/src/main/java/org/weatherook/weatherook/ui/activity/MainActivity.kt

--- app/src/main/java/org/weatherook/weatherook/ui/fragment/filter/FilterFragment.kt
+++ app/src/main/java/org/weatherook/weatherook/ui/fragment/filter/FilterFragment.kt
@@ -4,14 +4,24 @@ import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.text.Html
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import kotlinx.android.synthetic.main.filter_today.*
 import kotlinx.android.synthetic.main.fragment_filter.*
 import org.weatherook.weatherook.R
 
 class FilterFragment :  Fragment(), View.OnClickListener{
     override fun onClick(p0: View?) {
+
+        var gender :String = "여"
+        var tall : Int = 160
+        var size : String = "1"
+        var sizelist = arrayOf("마름","보통","통통", "뚱뚱")
+        var stylelist : ArrayList<String> = ArrayList()
+        var today_style: ArrayList<TextView> = ArrayList()
 
         when(p0) {
             filter_today -> {
@@ -33,7 +43,31 @@ class FilterFragment :  Fragment(), View.OnClickListener{
             }
             filter_ok -> {
                 val fragmentTransaction = fragmentManager!!.beginTransaction()
-                fragmentTransaction.replace(R.id.filter_com_frame, FilterCompleteFragment()).commit()
+                fragmentTransaction.replace(R.id.filter_com_frame, FilterCompleteFragment(),"filtercomplete").commit()
+                if(!today_women.isSelected == true) gender = "남"
+                else gender = "여"
+                tall = today_spinner_tall.selectedItem.toString().toInt()
+                // tall = today_spinner_tall.selectedItemPosition.toString().toInt()
+                size = sizelist[today_size_seekbar.progress]
+                today_style.add(today_style_btn1)
+                today_style.add(today_style_btn2)
+                today_style.add(today_style_btn3)
+                today_style.add(today_style_btn4)
+                today_style.add(today_style_btn5)
+                today_style.add(today_style_btn6)
+                today_style.add(today_style_btn7)
+                today_style.add(today_style_btn8)
+                today_style.add(today_style_btn9)
+                today_style.add(today_style_btn10)
+//
+                for (ts in today_style) {
+                    if(ts.isSelected){
+                        stylelist.add(ts.text.toString())
+                    }
+                }
+                Log.d("tag", "============================ : " + gender + tall+ size + stylelist+ "=============================")
+
+
             }
         }
 
